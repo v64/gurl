18f4fbc40eb2f62fc9846e64e46239f09041e412
Jahn Veach <j@hnvea.ch>
2013-09-10 02:29:23 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+Gurl: A quick, multithreaded URL downloader.
+
+To run: go run gurl.go
+
+Expects a CSV in the same directory named urls.csv that looks like this:
+"http://www.example.com/img/img1.jpg"
+"http://www.example2.com/img2.jpg"
+"http://host1.www.examples.com/images/img/imag2.jpeg"
+
+and downloads the files and saves them this way:
+./output/www.example.com/httpwwwexamplecomimgimg1.jpg
+./output/www.example2.com/httpwwwexample2comimg2.jpg
+./output/host1.www.examples.com/httphost1wwwexamplescomimagesimgimag2.jpeg
---
+++ gurl.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+func main() {
+	// Create a wait group to report to and a work channel that takes URLs
+	wg := new(sync.WaitGroup)
+	work := make(chan string)
+
+	// Create workers and start goroutines listening to work channel
+	numWorkers := 100
+	wg.Add(numWorkers) // 100 workers
+	for i := 1; i <= numWorkers; i++ {
+		go worker(work, wg)
+	}
+
+	// Add URLs to work channel from CSV
+	addToWork(work)
+
+	// Wait for workers to finish
+	close(work)
+	wg.Wait()
+
+	fmt.Println("Done.")
+}
+
+func addToWork(work chan string) {
+	// Open the file
+	file, err := os.Open("urls.csv")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+
+	for {
+		line, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			break
+		}
+
+		work <- line[0]
+	}
+}
+
+func worker(work chan string, wg *sync.WaitGroup) {
+	for workUrl := range work {
+		fmt.Println("Getting:", workUrl)
+
+		u, err := url.Parse(workUrl)
+		if err != nil {
+			fmt.Println("Invalid URL, skipping.")
+			continue
+		}
+
+		ext := path.Ext(u.Path)
+		name := sanitizeUrl(u.Scheme + u.Host + strings.TrimSuffix(u.Path, ext) + "?" + u.RawQuery)
+		filePath := "./output/" + u.Host + "/"
+		fileName := filePath + name + ext
+		fmt.Println("Saving:", fileName)
+
+		err = os.MkdirAll(filePath, 0755)
+		if err != nil {
+			fmt.Println("Error creating directory, skipping:", err)
+			continue
+		}
+
+		out, err := os.Create(fileName)
+		if err != nil {
+			fmt.Println("Error creating file, skipping:", err)
+			continue
+		}
+		defer out.Close()
+
+		resp, err := http.Get(workUrl)
+		if err != nil {
+			fmt.Println("Error getting URL, skipping:", err)
+			continue
+		}
+		defer resp.Body.Close()
+
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			fmt.Println("Error writing file, skipping:", err)
+			continue
+		}
+	}
+
+	wg.Done()
+}
+
+func sanitizeUrl(url string) string {
+	sanitized := stripChars(url)
+
+	whitespace := regexp.MustCompile("\\s+")
+	sanitized = whitespace.ReplaceAllLiteralString(sanitized, "-")
+
+	return sanitized
+}
+
+func stripChars(str string) string {
+	toStrip := [...]string{"~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;", "—", "–", ",", "<", ".", ">", "/", "?"}
+
+	for i := range toStrip {
+		str = strings.Replace(str, toStrip[i], "", -1)
+	}
+
+	return str
+}
